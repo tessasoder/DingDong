71a6f7fe2a8dce1aa5dfaaf7bb41ab54c8d8227d
Philipp Steiner <philippsteiner.go@gmail.com>
2017-11-24 09:14:07 +0100

    login new

--- project_dingdong/project_dingdong/Login.cs
+++ project_dingdong/project_dingdong/Login.cs
@@ -15,70 +15,57 @@ namespace project_dingdong
     public partial class Login : Form
     {
 
+        MySqlConnection con = new MySqlConnection("server=eduweb.kb.local;username=team09;password=T3amO9;database=team09;");
+        int i;
         public Login()
         {
             InitializeComponent();
-
-            //lalalalla i bims 1 kommentar
-           
         }
 
-        public static List<string> uname = new List<string>();
-        public static List<string> pw = new List<string>();
-        public static MySqlConnection con = new MySqlConnection("server=eduweb.kb.local;username=team09;password=T3amO9;database=team09;");
-        //test login db in progress
-        private  void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            
-           
-            MySqlCommand len = new MySqlCommand("select count(username) from Login;", con);
-            
-            if(tb_pass.Text == "" || tb_usern.Text == "")
+            try
             {
-                MessageBox.Show("Bitte Username und Passwort eingeben!", "Achtung",MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                con.Open();
+                MySqlCommand cmd = con.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Login where username = '" + tb_usern.Text + "' and password= '" + tb_pass.Text + "'";
+                cmd.ExecuteNonQuery();
+                DataTable dt = new DataTable();
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                da.Fill(dt);
+                i = Convert.ToInt32(dt.Rows.Count.ToString());
+                Console.WriteLine(i);
+
+                //login
+                if (i == 0)
+                {
+                    MessageBox.Show("fail");
+                    Console.WriteLine("login failed: " + tb_usern.Text + ";" + tb_pass.Text + "");
+                }
+                //admin login
+                else if (i != 0 && tb_usern.Text == "admin")
+                {
+                    AdminStart a = new AdminStart();
+                    Console.WriteLine("logged in as admin");
+                    this.Hide();
+                    a.Show();
+                }
+                //emp login
+                else
+                {
+                    EmpStart em = new EmpStart();
+                    Console.WriteLine("logged in as emplyee");
+                    this.Hide();
+                    em.Show();
+                }
+
+                con.Close();
             }
-            else
+            catch (Exception ex)
             {
-                try
-                {
-                    con.Open();
-                    Console.WriteLine("con success");
-                    for (int i = 0; i < 1; i++)
-                    {
-                        Console.WriteLine(i);
-                    }
-                    Console.WriteLine();
-                    con.Close();
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex);
-                }
+                Console.WriteLine(ex);
             }
-           
-
-
-            //////////////////////////////////////////////////////////
-            //login without db
-            //if (tb_usern.Text == "admin" && tb_pass.Text == "123")
-            //{
-            //    AdminStart a = new AdminStart();
-            //    Console.WriteLine("logged in as admin");
-            //    this.Hide();
-            //    a.Show();
-
-            //}
-            //else if(tb_usern.Text == "emp" && tb_pass.Text == "123")
-            //{
-            //    EmpStart em = new EmpStart();
-            //    Console.WriteLine("logged in as emplyee");
-            //    this.Hide();
-            //    em.Show();
-
-            //}
-            //else
-            //    MessageBox.Show("user not found");
-            //////////////////////////////////////////////////////////
         }
 
         private void tb_pass_TextChanged(object sender, EventArgs e)
