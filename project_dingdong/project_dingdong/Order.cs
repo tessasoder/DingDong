09050cb91d444cc672ea7eb72c63bb9ce7ddc1ce
Philipp Steiner <philippsteiner.go@gmail.com>
2018-02-09 10:01:32 +0100

    orders

--- project_dingdong/project_dingdong/Order.cs
+++ project_dingdong/project_dingdong/Order.cs
@@ -19,6 +19,8 @@ namespace project_dingdong
         MySqlCommand getdrink = Login.con.CreateCommand();
         string cat = "";
         int num = 0;
+        decimal total = 0;
+
         string[] cats = { "beer", "wine", "non_a", "shots", "mixed", "special" };
 
         //lists
@@ -28,6 +30,11 @@ namespace project_dingdong
         List<string> shots = new List<string>();
         List<string> mixed = new List<string>();
         List<string> special = new List<string>();
+        
+        //0 = names
+        //1 = prices
+        //NEXT STEP: create method which creates a bill with name of drink and price -> beer, beer  = 2x beer 
+        List<object>[] bill = new List<object>[2];
         #endregion
 
         public Order()
@@ -66,7 +73,7 @@ namespace project_dingdong
                     num = count(cat);
 
                     //create buttons
-                    createbtns(cat, num);
+                    createbtns(cat, num, bill);
 
                 }
 
@@ -160,21 +167,6 @@ namespace project_dingdong
             return sb.ToString();
         }
 
-
-        ////delete buttons
-        //private void delotherbtns()
-        //{
-        //    foreach (Control btn in this.Controls)
-        //    {
-        //        if (btn.Name.Contains("$"))
-        //        {
-        //            this.Controls.Remove(btn);
-        //            Console.WriteLine("del "+ btn.Name);
-        //        }
-                    
-        //    }
-        //}
-
         //count objects in category
         private int count(string cat)
         {
@@ -185,56 +177,80 @@ namespace project_dingdong
         }
 
         //create buttons for category
-        private void createbtns(string cat, int num)
+        private void createbtns(string cat, int num, List<object>[] prices)
         {
 
-
+            string name;
+            decimal price;
             Console.WriteLine("\nCategory: "+ cat);
             
             getdrink.CommandText = "SELECT * FROM `Drinks` WHERE category = '" + cat + "'";
 
             MySqlDataReader reader = getdrink.ExecuteReader();
             DataTable table = reader.GetSchemaTable();
-
-           while (reader.Read())
-           {
-                
-                Console.WriteLine("{0}\n{1}", reader.GetString(0),reader.GetDecimal(1));
-
-            }
-           reader.Close();
-
-
-
-
-
-
-
-            int top = 400;
-            for (int i = 0; i < num; i++)
+            int top = 250;
+            int counter = 0;
+            while (reader.Read())
             {
+                
+                name = reader.GetString(1);
+                price = reader.GetDecimal(2);
+                Console.WriteLine("{0}\n{1}", name,price);
 
-                //Console.WriteLine(i);
+                
+
                 Button btn = new Button();
 
-                btn.Text = cat;
-                btn.Name = "$" + cat + "_" + i;
-                btn.Left = 50;
-                btn.Top = top;
-                btn.Click += (s, e) =>
-                {  //delgation
-                    MessageBox.Show(btn.Name);
 
-                };
+                //button positioning
+                if (counter == 4)
+                {
+                    btn.Left = 220;
+                    top = 250;
+                }
+                else if(counter > 4)
+                    btn.Left = 220;
+                else
+                    btn.Left = 50;
 
-                top += btn.Height + 2;
+                   
+                    btn.Text = name;
+                    btn.Name = "$" + cat + "_" + name;
+                    btn.Top = top;
+                    btn.Height = 60;
+                    btn.Width = 140;
+
+                    //btn click event
+                    btn.Click += (s, e) =>
+                    {  //delgation
+                        //MessageBox.Show(btn.Name);
+                        money(price);
+                        
+                    };
+
+                top = top += btn.Height + 10;
                 //Console.WriteLine(btn.Name);
                 btn.Visible = false;
 
-                this.Controls.Add(btn);
-
+                    this.Controls.Add(btn);
+                Console.WriteLine(top.ToString());
+                counter++;
             }
+           reader.Close();
+
+           
+
+            //name
+            //price
+            //
+
+
+
+
+           
     }
+
+       
 
         //handle visible buttons
         private void vsbltyHandler(string cat)
@@ -253,13 +269,36 @@ namespace project_dingdong
             }
         }
 
+
+        private void money(decimal price)
+        {
+           bill[1].Add(price);
+            
+            total += price;
+            
+            lbl_total.Text = total.ToString();
+        }
+
         private void loadLists(string[] cats)
         {
             
         }
+
+
+
         #endregion
 
+        private void btn_save_Click(object sender, EventArgs e)
+        {
 
+            Console.WriteLine("\nBiLL:");
+            for (int i = 0; i < bill[1].Count(); i++)
+            {
+                Console.WriteLine(bill[1][i].ToString());
+                
+            }
 
+           
+        }
     }
 }
